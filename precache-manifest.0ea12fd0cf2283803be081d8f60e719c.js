d78affe47305c174db8ea5f3799d167c2f729a40
David Cámara Iglesias <david.c.iglesias@gmail.com>
2018-10-10 16:35:35 +0200

    Updates

---
+++ precache-manifest.0ea12fd0cf2283803be081d8f60e719c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tutorial/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "a10997ba4f0052e7204e",
+    "url": "/react-tutorial/static/js/main.a10997ba.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-tutorial/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "a10997ba4f0052e7204e",
+    "url": "/react-tutorial/static/css/main.46d40f03.chunk.css"
+  },
+  {
+    "revision": "c579ad90968f25ece7be95ddeb312ee4",
+    "url": "/react-tutorial/index.html"
+  }
+];
