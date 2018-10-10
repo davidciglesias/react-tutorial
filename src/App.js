831151719854c9fc900231c030f6f94bc27bdc4d
David Cámara Iglesias <david.c.iglesias@gmail.com>
2018-10-10 17:08:58 +0200

    Cats!

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            I'm already learning React!
           </a>
         </header>
       </div>
